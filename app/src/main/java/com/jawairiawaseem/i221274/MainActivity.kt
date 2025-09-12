0469e6e8c1199b009a8dcbe65b39d68ba24a09b9
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-12 13:58:43 +0500

    Splash activity added

--- app/src/main/java/com/jawairiawaseem/i221274/MainActivity.kt
+++ app/src/main/java/com/jawairiawaseem/i221274/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.jawairiawaseem.i221274
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main) // your splash layout
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity2::class.java))
+            finish() // close splash so user cannot go back to it
+        }, 2000) // delay in ms
     }
+
 }
--- app/src/main/java/com/jawairiawaseem/i221274/MainActivity2.kt
+++ app/src/main/java/com/jawairiawaseem/i221274/MainActivity2.kt
@@ -1,20 +1,26 @@
 package com.jawairiawaseem.i221274
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity2 : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val signupText = findViewById<TextView>(R.id.tvsignup)
+            signupText.setOnClickListener {
+                val intent = Intent(this, signupActivity::class.java)
+                startActivity(intent)
         }
     }
 }
