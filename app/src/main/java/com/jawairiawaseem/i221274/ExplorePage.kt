f52e4d0529f5d38852cb0fe15307edcd639056a7
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-13 23:03:09 +0500

    page 6

--- app/src/main/java/com/jawairiawaseem/i221274/ExplorePage.kt
+++ app/src/main/java/com/jawairiawaseem/i221274/ExplorePage.kt
@@ -2,7 +2,6 @@ package com.jawairiawaseem.i221274
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -10,19 +9,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeScreen : AppCompatActivity() {
+class ExplorePage : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_screen)
+        setContentView(R.layout.activity_explore_page)
+        val redhomebtn = findViewById<ImageView>(R.id.redhomebtn)
 
-        val explore = findViewById<ImageView>(R.id.explore)
-         explore.setOnClickListener {
-             val intent= Intent(this, ExplorePage::class.java)
-             startActivity(intent)
-         }
-
+        redhomebtn.setOnClickListener {
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
