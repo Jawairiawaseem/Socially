5e8f062e45227dc4b14a694fcf54077dcc4f7519
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 17:17:17 +0500

    12 pages

--- app/src/main/java/com/jawairiawaseem/i221274/profilePage.kt
+++ app/src/main/java/com/jawairiawaseem/i221274/profilePage.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,34 @@ class profilePage : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_page)
         val editProfilebtn =findViewById<Button>(R.id.editProfilebtn)
+        val h1=findViewById<ImageView>(R.id.h1)
+        val h2=findViewById<ImageView>(R.id.h2)
+        val h3=findViewById<ImageView>(R.id.h3)
+
+        val homeiv = findViewById<ImageView>(R.id.homeiv)
+        homeiv.setOnClickListener {
+            val intent= Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        }
+        val exploreiv = findViewById<ImageView>(R.id.exploreiv)
+        exploreiv.setOnClickListener {
+            val intent = Intent(this, ExplorePage::class.java)
+            startActivity(intent)
+        }
+
+        h1.setOnClickListener {
+            val intent= Intent(this, highlight::class.java)
+            startActivity(intent)
+        }
+        h2.setOnClickListener {
+            val intent= Intent(this, highlight::class.java)
+            startActivity(intent)
+        }
+
+        h3.setOnClickListener {
+            val intent= Intent(this, highlight::class.java)
+            startActivity(intent)
+        }
         editProfilebtn.setOnClickListener {
             val intent= Intent(this, EditProfile::class.java)
             startActivity(intent)
