43aa062ebf74adbc54a3bd71c82d884246cab14c
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 21:50:13 +0500

    19 pages

--- app/src/main/java/com/jawairiawaseem/i221274/ProfileFollow.kt
+++ app/src/main/java/com/jawairiawaseem/i221274/ProfileFollow.kt
@@ -1,25 +1,16 @@
 package com.jawairiawaseem.i221274
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Profile : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class ProfileFollow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-
-        val backarrow = findViewById<ImageView>(R.id.backarrow)
-        backarrow.setOnClickListener {
-            finish()
-        }
-
+        setContentView(R.layout.activity_profile_follow)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
