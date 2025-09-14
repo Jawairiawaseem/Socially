b4797845a6e18e255869399b08dee3ab21484f2a
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 10:51:29 +0500

    page 8

---
+++ app/src/main/java/com/jawairiawaseem/i221274/chat1.kt
@@ -0,0 +1,28 @@
+package com.jawairiawaseem.i221274
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class chat1 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chat1)
+        val backbtn= findViewById<ImageView>(R.id.backbtn)
+
+        backbtn.setOnClickListener {
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
