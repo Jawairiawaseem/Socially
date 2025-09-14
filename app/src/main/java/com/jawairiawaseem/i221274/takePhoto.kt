c4f9f4d100d50e31da1f606d59ac6ff024bd7d47
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 19:02:46 +0500

    14 pages

---
+++ app/src/main/java/com/jawairiawaseem/i221274/takePhoto.kt
@@ -0,0 +1,27 @@
+package com.jawairiawaseem.i221274
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class takePhoto : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_take_photo)
+        val arrowiv = findViewById<ImageView>(R.id.arrowiv)
+        arrowiv.setOnClickListener { finish()   }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
