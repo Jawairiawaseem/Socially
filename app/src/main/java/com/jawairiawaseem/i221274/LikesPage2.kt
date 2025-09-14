6269f235368dfd656775669d16f4b88258d61e4f
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 23:02:21 +0500

    All pages

---
+++ app/src/main/java/com/jawairiawaseem/i221274/LikesPage2.kt
@@ -0,0 +1,20 @@
+package com.jawairiawaseem.i221274
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LikesPage2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_likes_page2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
