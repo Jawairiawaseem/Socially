20dc5b86989de61b66ae3b468a61a1973c0a2579
Jawairiawaseem <i221274@nu.edu.pk>
2025-09-14 20:09:52 +0500

    16 pages

---
+++ app/src/main/java/com/jawairiawaseem/i221274/AddStory.kt
@@ -0,0 +1,32 @@
+package com.jawairiawaseem.i221274
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddStory : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_story)
+
+        val closeiv = findViewById<ImageView>(R.id.closeiv)
+        val nextiv = findViewById<ImageView>(R.id.nextiv)
+        nextiv.setOnClickListener {
+            finish()
+        }
+        closeiv.setOnClickListener {
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
